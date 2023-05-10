0749bbeaff48ad0d807e319ada7e8a1f4305f43b
jorjor <jorjor1230@gmail.com>
2023-05-10 20:54:53 +0800

    Add files via upload

--- test.py
+++ test.py
@@ -1,5 +1,10 @@
-import numpy as np
+from sympy import ntt, intt
 
-a = np.array([1, 2, 3, 4])
-a *= 2
-print(np.fft.ifft(a))
+a = [100, 200, 200, 0, 100, 200, 200, 0]
+b = [100, 200, 300, 400]
+
+a_ntt = ntt(a, 3329)
+a_intt = intt(a_ntt, 3329)
+print(a_ntt)
+print(a_intt)
+print(ntt(b, 3329))
